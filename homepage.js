77321e0a29fccd6348c7b5aeb63c4987bc4b4b5c
KojieSann <109472332+KojieSann@users.noreply.github.com>
2024-08-01 17:27:55 +0800

    ( ｡ •̀ ⤙ •́ ｡ )

--- homepage.js
+++ homepage.js
@@ -1,55 +1,108 @@
-$("ul.main-menu li").click(function (e) {
-  if ($(this).siblings("li").find("ul.submenu:visible").length) {
-    $("ul.submenu").slideUp("normal");
-  }
-  $(this).find("ul.submenu").slideToggle("normal");
-});
-var tl = new TimelineMax({ paused: true });
-tl.to(".menu", 0.8, {
-  autoAlpha: 1,
-});
-tl.staggerFrom(
-  ".main-menu li a:not(.submenu li a)",
-  1,
-  {
+Splitting();
+
+function navigation() {
+  $("ul.main-menu li").click(function (e) {
+    if ($(this).siblings("li").find("ul.submenu:visible").length) {
+      $("ul.submenu").slideUp("normal");
+    }
+    $(this).find("ul.submenu").slideToggle("normal");
+  });
+  var tl = new TimelineMax({ paused: true });
+  tl.to(".menu", 0.8, {
+    autoAlpha: 1,
+  });
+  tl.staggerFrom(
+    ".main-menu li a:not(.submenu li a)",
+    1,
+    {
+      opacity: 0,
+      y: 10,
+      ease: Power3.easeInOut,
+    },
+    0.1
+  );
+  tl.from(".submenu", 0.8, {
+    autoAlpha: 0,
+  });
+  tl.staggerFrom(
+    ".media-nav ul li",
+    1,
+    {
+      opacity: 0,
+      y: 10,
+      ease: Power3.easeInOut,
+    },
+    0.1,
+    "-=2"
+  );
+  tl.from(".call", 1, {
+    delay: -2,
     opacity: 0,
     y: 10,
     ease: Power3.easeInOut,
-  },
-  0.1
-);
-tl.from(".submenu", 0.8, {
-  autoAlpha: 0,
-});
-tl.staggerFrom(
-  ".media-nav ul li",
-  1,
-  {
+  });
+  tl.from(".mail-nav", 1, {
+    delay: -1.6,
     opacity: 0,
     y: 10,
     ease: Power3.easeInOut,
+  });
+  tl.reverse();
+  $(document).on("click", ".menu-btn", function () {
+    tl.reversed(!tl.reversed());
+  });
+  $(document).on("click", ".close-menu", function () {
+    tl.reversed(!tl.reversed());
+  });
+}
+navigation();
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.to(".oct-logo", {
+  scrollTrigger: {
+    trigger: ".oct-logo",
+    start: "top 60%",
+    end: "bottom 33%",
+    pin: true,
+
+    scrub: true,
   },
-  0.1,
-  "-=2"
-);
-tl.from(".call", 1, {
-  delay: -2,
+  scale: 2.3,
+  opacity: 0.1,
+  y: 0,
+  ease: "slow",
+  duration: 2,
+});
+
+const gtl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".title-intro",
+    start: "top 80%",
+    end: "top 30%",
+    scrub: true,
+    toggleActions: "play none none reset",
+  },
+});
+
+gtl.from(".title-intro .char", {
+  duration: 1,
   opacity: 0,
-  y: 10,
-  ease: Power3.easeInOut,
+  yPercent: 130,
+  stagger: 0.09,
+  ease: "back.out",
 });
-tl.from(".mail-nav", 1, {
-  delay: -1.6,
-  opacity: 0,
-  y: 10,
-  ease: Power3.easeInOut,
-});
-tl.reverse();
-$(document).on("click", ".menu-btn", function () {
-  tl.reversed(!tl.reversed());
-});
-$(document).on("click", ".close-menu", function () {
-  tl.reversed(!tl.reversed());
+
+gsap.from(".question-mark", {
+  scrollTrigger: {
+    trigger: ".introduction",
+    start: "top 70%",
+    end: "top top",
+    scrub: true,
+  },
+  rotation: 180,
+  x: 400,
+  duration: 1,
+  ease: "bounce",
 });
 
 let items = document.querySelectorAll(".slider .list .item");
@@ -92,6 +145,7 @@ document.querySelectorAll(".question-wrapper").forEach((wrapper) => {
     wrapper.classList.toggle("expanded");
   });
 });
+
 let currentScroll = 0;
 let isScrollingDown = true;
 let arrows = document.querySelectorAll(".arrow");
