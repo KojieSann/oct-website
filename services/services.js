3f5edcdf766021862494daf415b9032a3b6e20eb
KojieSann <109472332+KojieSann@users.noreply.github.com>
2024-10-16 14:14:08 +0800

    contact

--- services/services.js
+++ services/services.js
@@ -60,6 +60,88 @@ function navigation() {
 }
 navigation();
 
+function validateEmail(formType) {
+  const emailInput = document.getElementById(`email-field-${formType}`);
+  const emailError = document.getElementById(
+    `emailError${formType.charAt(0).toUpperCase() + formType.slice(1)}`
+  );
+  const emailPattern =
+    /^[A-Za-z._\-0-9]+@[A-Za-z]+\.[A-Za-z]{2,}(\.[A-Za-z]{2,})?$/;
+
+  if (!emailInput.value) {
+    emailError.innerHTML =
+      "<i class='fa-solid fa-circle-xmark'></i> Email field cannot be empty";
+    emailInput.style.borderBottomColor = "red";
+    return false;
+  }
+
+  if (!emailPattern.test(emailInput.value)) {
+    emailError.innerHTML =
+      "<i class='fa-solid fa-circle-xmark'></i> Please enter a valid email address";
+    emailInput.style.borderBottomColor = "red";
+    return false;
+  }
+
+  emailError.innerHTML = "";
+  emailInput.style.borderBottomColor = "green";
+  return true;
+}
+
+function validateMessage(formType) {
+  const messageField = document.getElementById(
+    `messageField${formType.charAt(0).toUpperCase() + formType.slice(1)}`
+  );
+  const messageError = document.getElementById(
+    `messageError${formType.charAt(0).toUpperCase() + formType.slice(1)}`
+  );
+
+  if (!messageField.value) {
+    messageError.innerHTML =
+      "<i class='fa-solid fa-circle-xmark'></i> Message cannot be empty";
+    messageField.style.borderBottomColor = "red";
+    return false;
+  }
+
+  messageError.innerHTML = "";
+  messageField.style.borderBottomColor = "green";
+  return true;
+}
+
+function handleSubmit(formType) {
+  const isEmailValid = validateEmail(formType);
+  const isMessageValid = validateMessage(formType);
+
+  if (isEmailValid && isMessageValid) {
+    document
+      .getElementById(
+        `contactForm${formType.charAt(0).toUpperCase() + formType.slice(1)}`
+      )
+      .submit();
+  }
+}
+
+const collegeContact = document.querySelector(".modalBG-College");
+const collegeBtn = document.querySelector(".collegeBtn");
+const collegeClose = document.querySelector(".closeCollege");
+
+collegeBtn.addEventListener("click", () => {
+  collegeContact.classList.add("showContact");
+});
+collegeClose.addEventListener("click", () => {
+  collegeContact.classList.remove("showContact");
+});
+
+const shsContact = document.querySelector(".modalBG-shs");
+const shsBtn = document.querySelector(".shsBtn");
+const shsClose = document.querySelector(".closeShs");
+
+shsBtn.addEventListener("click", () => {
+  shsContact.classList.add("showContactSHS");
+});
+shsClose.addEventListener("click", () => {
+  shsContact.classList.remove("showContactSHS");
+});
+
 const openNav = document.querySelector(".toggle .icon");
 openNav.addEventListener("click", () => {
   const container = document.querySelector(".navigation-container");
